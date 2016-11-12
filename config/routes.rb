3c66768b52c2e341078f74c145df5dc05b031d59
Yohei Senju <yohei.senju@gmail.com>
2016-11-12 21:54:05 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'members', to: 'welcome#members'
   get 'contact', to: 'welcome#contact'
+  get 'sample', to: 'welcome#sample'
   get 'link', to: 'welcome#link'
 
 
